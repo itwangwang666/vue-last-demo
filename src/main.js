a7a1b19c73aeaa1094fa55b8bebf3c8909850e48
wangzhai <1215136016@qq.com>
2019-02-24 22:25:42 +0800

    项目架构搭建完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//导入reset.css
+import './styles/common.css'
+
+//测试按需引入
+import { Button } from 'vant';
+Vue.use(Button)
+
 Vue.config.productionTip = false
 
 new Vue({
