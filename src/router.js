60e5c341103cb4391088522162654b2b25e23903
wangzhai <1215136016@qq.com>
2019-02-25 14:48:45 +0800

    tabbar完成

--- src/router.js
+++ src/router.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import homeComponent from './pages/home'
+import classifyComponent from './pages/classify'
+import newsComponent from './pages/news'
+import shopCarComponent from './pages/shopCar'
+import myComponent from './pages/my'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/', redirect:'/home' },
-    { path: '/home', component: homeComponent }
+    { path: '/home', component: homeComponent },
+    { path: '/classify', component: classifyComponent },
+    { path: '/news', component: newsComponent },
+    { path: '/shopCar', component: shopCarComponent },
+    { path: '/my', component: myComponent },
   ]
 })
