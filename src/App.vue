f7cd657bf09f2c562fbf94419554780f33284360
wangzhai <1215136016@qq.com>
2019-02-25 11:46:47 +0800

    tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div id="app">
     <router-view/>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="wap-home">首页 </van-tabbar-item>
+      <van-tabbar-item icon="orders-o">分类</van-tabbar-item>
+      <van-tabbar-item icon="search">新闻</van-tabbar-item>
+      <van-tabbar-item icon="shopping-cart-o">购物车</van-tabbar-item>
+      <van-tabbar-item icon="contact">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+      icon: {
+        normal: "//img.yzcdn.cn/icon-normal.png",
+        active: "//img.yzcdn.cn/icon-active.png"
+      }
+    };
+  }
+};
+</script>
 <style lang="less">
-
 </style>
